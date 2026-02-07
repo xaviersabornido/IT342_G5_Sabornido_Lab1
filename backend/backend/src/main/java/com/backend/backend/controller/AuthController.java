295c56c25e361415e90f42ed74b7f2dbae5c3b34
Xavier Sabornido <xaviersabornido2004@gmail.com>
2026-02-07 11:47:40 +0800

    fix: remove unused import AuthController.

--- backend/backend/src/main/java/com/backend/backend/controller/AuthController.java
+++ backend/backend/src/main/java/com/backend/backend/controller/AuthController.java
@@ -5,8 +5,6 @@ import com.backend.backend.service.AuthService;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
